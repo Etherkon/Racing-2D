6d0064c27478c799b2273399331e2f2516ff2ab3
Petri Mattila <Etherkon@users.noreply.github.com>
2018-04-06 14:01:14 +0300

    Create manager.cs

---
+++ Scripts/manager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class manager : MonoBehaviour {
+
+	
+
+public static int bottas;
+public static int hamilton;
+public static int vettel;
+public static int raikkonen;
+
+public static int bot;
+public static int ham;
+public static int vet;
+public static int rai;
+
+public static int race;
+
+void Start () {
+		
+	
+	bottas = 0;
+	hamilton = 0;
+	vettel = 0;
+	raikkonen = 0;
+	race = 0;
+	DontDestroyOnLoad(this);
+}
+	
+	
+
+}
